483769518d4d217a92d1328c072d51273c0a5f47
Scotty Franzyshen <sfranzyshen@gmail.com>
2020-09-15 14:42:41 -0700

    Update Otto_allmoves_V9.js

--- Otto_allmoves_V9.js
+++ Otto_allmoves_V9.js
@@ -11,22 +11,22 @@ Otto.walk(2, 1000, 1);           #-- 2 steps, "TIME". IF HIGHER THE VALUE THEN S
 Otto.walk(2, 1000, -1);					#-- 2 steps, T, -1 BACKWARD 
 Otto.turn(2, 1000, 1);					  #-- 3 steps turning LEFT
 Otto.home();
-time.sleep_ms(100);  
+Otto.sleep_ms(100);  
 Otto.turn(2, 1000, -1);					#-- 3 steps turning RIGHT 
 Otto.bend(1, 500, 1);						#-- usually steps =1, T=2000
 Otto.bend(1, 2000, -1);     
 Otto.shakeLeg(1, 1500, 1);
 Otto.home();
-time.sleep_ms(100);
+Otto.sleep_ms(100);
 Otto.shakeLeg(1, 2000, -1);
 Otto.moonwalker(3, 1000, 25, 1); #-- LEFT
 Otto.moonwalker(3, 1000, 25,-1); #-- RIGHT  
 Otto.crusaito(2, 1000, 20, 1);
 Otto.crusaito(2, 1000, 20, -1);
-time.sleep_ms(100);
+Otto.sleep_ms(100);
 Otto.flapping(2, 1000, 20, 1);
 Otto.flapping(2, 1000, 20, -1);
-time.sleep_ms(100);
+Otto.sleep_ms(100);
 Otto.swing(2, 1000, 20);
 Otto.tiptoeSwing(2, 1000, 20);
 Otto.jitter(2, 1000, 20);				#-- (small T)
@@ -36,4 +36,4 @@ Otto.jump(1, 2000);
 
 Otto.home();
 
-//end
+
